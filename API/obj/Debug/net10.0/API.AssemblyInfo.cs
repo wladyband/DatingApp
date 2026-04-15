0d20415dd4ef91025f83c3c2a6fe76a14646d1d0
Wladimir Bandeira <wladimir.bandeira@br.ey.com>
2026-04-15 18:01:45 -0300

    criando migração para as entidades

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab7eb5b3f48be6e4aade33d70741bbc03936391d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b9a16c0b64d8c1b63011947fddba7271599bec3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
