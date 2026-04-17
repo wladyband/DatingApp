f77d52f07d958f4231ea94a4c4283eef92d031ab
Wladimir Bandeira <wladimir.bandeira@br.ey.com>
2026-04-17 05:58:27 -0300

    criando certificado para aplicação roda como https

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffefd92fa679b343f05f18c8907442ae8c36bdca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32b7bf6575866dc21f254dad9fad48b4d747da8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
