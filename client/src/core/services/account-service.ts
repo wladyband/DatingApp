1fb55e9defbc497b655234638da8d684bde7c4f0
Wladimir Bandeira <wladimir.bandeira@br.ey.com>
2026-04-29 06:06:50 -0300

    primeira parte de implementação da persistencia do login

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { User } from '../../types/user';
+import { tap } from 'rxjs/internal/operators/tap';
 
 @Injectable({
   providedIn: 'root',
@@ -8,10 +10,22 @@ import { inject, Injectable } from '@angular/core';
 export class AccountService {
 
   private http = inject(HttpClient);
+  currentUser = signal<User | null>(null);
 
-  baseUrl = 'http://localhost:5001/api/';
+  baseUrl = 'https://localhost:5001/api/';
 
   login(creds: any) {
-    return this.http.post(this.baseUrl + 'account/login', creds);
+    return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
+      tap(user => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user)
+        }
+      })
+    )
+  }
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
   }
 }
